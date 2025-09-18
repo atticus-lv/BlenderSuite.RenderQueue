dc742e1c12739e28de8c7b21798d2356f7c08770
1029920178@qq.com <1029910278@qq.com>
2025-09-19 01:11:12 +0800

    bring back exec

--- BlenderRenderQueue/Services/BlenderService/BasePythonProcessService.cs
+++ BlenderRenderQueue/Services/BlenderService/BasePythonProcessService.cs
@@ -93,7 +93,9 @@ public abstract class BasePythonProcessService : IDisposable
             progress?.Report(0.4);
 
             var wrappedScript = $@"
+exec('''
 {script}
+''')
 print('__SCRIPT_COMPLETE__')
 ";
 
