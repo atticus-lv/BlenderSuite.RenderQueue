5453fa6e32426fc22df0cea5de328a2f326fcacb
Atticus <1029910278@qq.com>
2025-09-18 19:15:32 +0800

    fix: dialog service selector

---
+++ BlenderRenderQueue/Views/Test/TestRenderView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BlenderRenderQueue.Views.Test;
+
+public partial class TestRenderView : UserControl
+{
+	public TestRenderView()
+	{
+		InitializeComponent();
+	}
+} 
