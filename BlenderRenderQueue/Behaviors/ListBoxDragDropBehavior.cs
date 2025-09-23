f5407e3800234386e085118a3a95723f02bb7110
1029920178@qq.com <1029910278@qq.com>
2025-09-23 11:48:14 +0800

    Revert "fix: set class too fast cause error"
    This reverts commit 61cd8836fe0822e625e0d4b6d49b89f33fc89425.

--- BlenderRenderQueue/Behaviors/ListBoxDragDropBehavior.cs
+++ BlenderRenderQueue/Behaviors/ListBoxDragDropBehavior.cs
@@ -228,13 +228,7 @@ public class ListBoxDragDropBehavior : Behavior<ListBox>
         {
             if (listBoxItem.DataContext == item)
             {
-                // 检查状态是否真的发生了变化
-                var currentState = listBoxItem.Classes.Contains("isDropTarget");
-                if (currentState != isDropTarget)
-                {
-                    Console.WriteLine($"[DragDrop Debug] SetDropTargetVisual: {System.IO.Path.GetFileName(item.BlendFilePath)} = {isDropTarget}");
-                    DropTargetBehavior.SetIsDropTarget(listBoxItem, isDropTarget);
-                }
+                DropTargetBehavior.SetIsDropTarget(listBoxItem, isDropTarget);
                 break;
             }
         }
@@ -249,13 +243,7 @@ public class ListBoxDragDropBehavior : Behavior<ListBox>
         {
             if (listBoxItem.DataContext == item)
             {
-                // 检查状态是否真的发生了变化
-                var currentState = listBoxItem.Classes.Contains("isDragTarget");
-                if (currentState != isDragTarget)
-                {
-                    Console.WriteLine($"[DragDrop Debug] SetDragTargetVisual: {System.IO.Path.GetFileName(item.BlendFilePath)} = {isDragTarget}");
-                    DropTargetBehavior.SetIsDragTarget(listBoxItem, isDragTarget);
-                }
+                DropTargetBehavior.SetIsDragTarget(listBoxItem, isDragTarget);
                 break;
             }
         }
