302a250028016e0fc36541fa86c55af06c6868d5
1029920178@qq.com <1029910278@qq.com>
2025-09-24 04:09:57 +0800

    fix: width not change

--- BlenderRenderQueue/Views/RenderQueueView.axaml.cs
+++ BlenderRenderQueue/Views/RenderQueueView.axaml.cs
@@ -1,13 +1,18 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using BlenderRenderQueue.ViewModels;
+using Avalonia;
 
 namespace BlenderRenderQueue.Views;
 
 public partial class RenderQueueView : UserControl
 {
+    private Border? _rightPanelBorder;
+    private TopLevel? _topLevel;
+
     public RenderQueueView()
     {
         InitializeComponent();
@@ -16,6 +21,55 @@ public partial class RenderQueueView : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+        
+        // 获取右侧面板的Border控件
+        _rightPanelBorder = this.FindControl<Border>("RightPanelBorder");
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        
+        // 获取TopLevel（窗口）
+        _topLevel = TopLevel.GetTopLevel(this);
+        if (_topLevel != null)
+        {
+            _topLevel.PropertyChanged += OnTopLevelPropertyChanged;
+            // 初始更新
+            UpdateRightPanelWidth();
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        if (_topLevel != null)
+        {
+            _topLevel.PropertyChanged -= OnTopLevelPropertyChanged;
+            _topLevel = null;
+        }
+        
+        base.OnDetachedFromVisualTree(e);
+    }
+
+    private void OnTopLevelPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        // 监听窗口的ClientSize变化
+        if (e.Property == TopLevel.ClientSizeProperty)
+        {
+            UpdateRightPanelWidth();
+        }
+    }
+
+    private void UpdateRightPanelWidth()
+    {
+        if (_rightPanelBorder != null && _topLevel != null)
+        {
+            var scale = 0.6;
+            var windowWidth = _topLevel.ClientSize.Width;
+            var calculatedWidth = windowWidth * scale;
+            
+            _rightPanelBorder.Width = calculatedWidth;
+        }
     }
 
     private void ToggleDeviceInfoPane(object? sender, RoutedEventArgs e)
@@ -26,6 +80,5 @@ public partial class RenderQueueView : UserControl
         {
             splitView.IsPaneOpen = !splitView.IsPaneOpen;
         }
-
     }
 }
