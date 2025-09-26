55d9dafe3de2fe1d874992711c9c992e0dc545de
1029920178@qq.com <1029910278@qq.com>
2025-09-27 00:40:49 +0800

    feat: add Localizer

---
+++ BlenderRenderQueue/Localizer/LocalizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace BlenderRenderQueue.Localizer;
+
+public class LocalizeConverter : IValueConverter
+{
+    public static readonly LocalizeConverter Instance = new LocalizeConverter();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string key)
+        {
+            return Localizer.Instance[key];
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
