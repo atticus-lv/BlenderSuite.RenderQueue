0a79e31d02b7d1df75602e217712f51e225bc002
1029920178@qq.com <1029910278@qq.com>
2025-10-03 22:11:28 +0800

    refactor: move blender exe card to new view

---
+++ BlenderRenderQueue/Views/BlenderSelectionView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BlenderRenderQueue.Views;
+
+public partial class BlenderSelectionView : UserControl
+{
+    public BlenderSelectionView()
+    {
+        InitializeComponent();
+    }
+}
