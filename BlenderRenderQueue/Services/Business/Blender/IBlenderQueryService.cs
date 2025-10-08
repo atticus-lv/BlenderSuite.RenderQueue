35194a69a9dab3e4b9136694732dac98f0f6ef08
1029920178@qq.com <1029910278@qq.com>
2025-10-08 14:09:45 +0800

    fix: 2 repeat query service

--- BlenderRenderQueue/Services/Business/Blender/IBlenderQueryService.cs
+++ BlenderRenderQueue/Services/Business/Blender/IBlenderQueryService.cs
@@ -2,14 +2,13 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using BlenderRenderQueue.Models;
-using BlenderRenderQueue.Services.Business.Blender.BlenderProcess;
 
 namespace BlenderRenderQueue.Services.Business.Blender;
 
 public interface IBlenderQueryService
 {
-    Task<(string ActiveScene, Dictionary<string, BlendSceneProperties> SceneData)> GetAllFilePropertiesAsync(
-        IBlenderProcess process,
+    Task<(string ActiveScene, Dictionary<string, BlendSceneProperties> SceneData)> GetAllFilePropertiesWithTempProcessAsync(
+        string blenderPath,
         string blendFilePath,
         CancellationToken cancellationToken = default);
 } 
